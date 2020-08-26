78e40e70c5c5254505483ec8cb8ec84e15a0b991
silversoulsless <doodlezzzfobozzz@gmail.com>
2020-08-26 19:55:33 +0300

    v 1.0.2 laser effects, spike+laser+gameover sounds, plarform collider fix

--- Assets/Scripts/AutoDestroy.cs
+++ Assets/Scripts/AutoDestroy.cs
@@ -10,6 +10,8 @@ public class AutoDestroy : MonoBehaviour
     private LaserDetector laserDetector = null;
     private AudioSource deathSound;
     private AudioSource bounceSound;
+    private AudioSource laserHit;
+    private AudioSource spikeHit;
     private int bounces = 0;
 
     private bool isSpiked = false;
@@ -19,6 +21,9 @@ public class AutoDestroy : MonoBehaviour
     {
         gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
         deathSound = GameObject.Find("Enemies").GetComponent<AudioSource>();
+
+        if(gameManager.currentLevelNum>=21)  laserHit = GameObject.Find("LaserSystem").GetComponent<AudioSource>();
+        if((gameManager.currentLevelNum>=11)||(gameManager.currentLevelNum<=20)) spikeHit = GameObject.Find("Spikes").GetComponent<AudioSource>();
         bounceSound = this.GetComponent<AudioSource>();
         if (PlayerPrefs.HasKey("effects"))
         {
@@ -68,6 +73,7 @@ public class AutoDestroy : MonoBehaviour
         {
             isSpiked = true;
             bounces = 0;
+            spikeHit.Play();
             if (gameManager.DestroyCount(this.gameObject) == gameManager.maxShootCount)
             {
                 if (gameManager.listOfEnemies.Count <= 0)
@@ -84,6 +90,7 @@ public class AutoDestroy : MonoBehaviour
         {
             isLasered = true;
             bounces = 0;
+            laserHit.Play();
             if (gameManager.DestroyCount(this.gameObject) == gameManager.maxShootCount)
             {
                 if (gameManager.listOfEnemies.Count <= 0)
